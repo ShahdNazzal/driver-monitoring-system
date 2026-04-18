e526ebd89cce52f134c411de72cd84ed95c01d33
Shahed Nazzal <shahednazzall.777@gmail.com>
2026-04-18 20:26:21 +0300

    Update model_utils.py

--- model_utils.py
+++ model_utils.py
@@ -1,6 +1,3 @@
-
-
-
 from classes import CLASSES
 
 def predict_behavior(model, img):
@@ -32,40 +29,3 @@ def predict_behavior(model, img):
         "behavior": str(behavior),
         "confidence": conf
     }
-
-
-
-    
-
-"""
-from classes import CLASSES
-
-def predict_behavior(model, img):
-
-    results = model(img)
-    result = results[0]
-
-    if result.boxes is None or len(result.boxes) == 0:
-        return {
-            "class_id": None,
-            "behavior": "No detection",
-            "confidence": None
-        }
-
-    best_idx = result.boxes.conf.argmax()
-
-    cls_id = int(result.boxes.cls[best_idx])
-    conf = float(result.boxes.conf[best_idx])
-
-    behavior = CLASSES.get(cls_id, f"unknown_{cls_id}")
-
-    return {
-        "class_id": cls_id,
-        "behavior": behavior,
-        "confidence": conf
-    }
-
-
-
-
-"""
